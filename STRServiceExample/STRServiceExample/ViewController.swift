94e732096a83aed128f1e87b0a00c0a3942b2ee0
Quyen Nguyen <ntquyen1@tma.com.vn>
2019-02-26 09:40:20 +0700

    no message

---
+++ STRServiceExample/STRServiceExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  STRServiceExample
+//
+//  Created by Quyen Nguyen The on 2/26/19.
+//  Copyright © 2019 Quyen Nguyen The. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
